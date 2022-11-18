7cd4058cc1716ca2af0597b26df51d324d797f24
Malte Baumann <1869741+maltebaumann@users.noreply.github.com>
2022-11-18 10:27:47 +0100

    IMGLYEngine version 1.9.0-preview.3

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "IMGLYEngine",
-      url: "https://cdn.img.ly/packages/imgly/engine-apple/1.9.0-preview.2/IMGLYEngine-v1.9.0-preview.2.xcframework.zip",
-      checksum: "e67644992b2f623ef963926b21f80859a8cbf82b44bb3348e6fc053259cc409f"
+      url: "https://cdn.img.ly/packages/imgly/engine-apple/1.9.0-preview.3/IMGLYEngine-v1.9.0-preview.3.xcframework.zip",
+      checksum: "19e3555a11fa0bb9bda33ec46a6297eb8f292271ec8e8f9280fd92c37ce45270"
     )
   ]
 )
