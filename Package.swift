dccb549dcc8dfb469b7eaec05e8f557cded6b010
Intergalactic Igor <malte.baumann+bot@img.ly>
2026-04-09 14:09:46 +0000

    IMGLYEngine version 1.72.2

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "IMGLYEngine",
-      url: "https://cdn.img.ly/packages/imgly/cesdk-swift/1.72.2-rc.0/IMGLYEngine-v1.72.2-rc.0.xcframework.zip",
-      checksum: "a6d10b42e8e3061bbaa41100cc2a5261fd66b49bc82c79a17d0a66afda8597af",
+      url: "https://cdn.img.ly/packages/imgly/cesdk-swift/1.72.2/IMGLYEngine-v1.72.2.xcframework.zip",
+      checksum: "9ae0222bd1c80fe0113fde1cc92f83a89898bc5eaf7cf604b04fd654c21f1a87",
     ),
   ],
 )
